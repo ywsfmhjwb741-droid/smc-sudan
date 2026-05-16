261d94b9a2346c4c90f265481bffab161ec363e5
Ahmed <ywsfmhjwb741@gmail.com>
2026-05-16 15:27:56 +0400

    fix: simplify trpc provider

--- apps/web/src/lib/trpc-provider.tsx
+++ apps/web/src/lib/trpc-provider.tsx
@@ -1,28 +1,14 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/client";
-import { createTRPCReact } from "@trpc/react-query";
 import { useState } from "react";
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const trpc = createTRPCReact<any>();
-
-function getBaseUrl() {
-  if (typeof window !== "undefined") return "";
-  if (process.env["VERCEL_URL"]) return `https://${process.env["VERCEL_URL"]}`;
-  return `http://localhost:${process.env["PORT"] ?? 3001}`;
-}
 
 export function TrpcProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
-  const [trpcClient] = useState(() =>
-    trpc.createClient({
-      links: [httpBatchLink({ url: `${getBaseUrl()}/api/trpc` })],
-    })
-  );
   return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </trpc.Provider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
   );
 }
+
+export const trpc = {} as any;
