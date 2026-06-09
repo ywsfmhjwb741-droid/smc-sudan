1c9f7eb4086224f3cf4c3ca27a92f62d0e739d3f
Ahmed <ywsfmhjwb741@gmail.com>
2026-06-09 13:30:04 +0400

    fix: add modules to TrpcModule

--- apps/api/src/trpc/trpc.module.ts
+++ apps/api/src/trpc/trpc.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
 import { TrpcRouter } from "./trpc.router";
 import { TrpcController } from "./trpc.controller";
+import { PlayersModule } from "../modules/players/players.module";
+import { LeaderboardModule } from "../modules/leaderboard/leaderboard.module";
 
 @Module({
+  imports: [PlayersModule, LeaderboardModule],
   controllers: [TrpcController],
   providers: [TrpcRouter],
 })
